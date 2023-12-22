28c72af461b654a6358165942f99134b17e53dea
unknown <Knifechik@gmail.com>
2023-12-22 15:28:54 +0300

    add new rules

--- internal/rules/builder.go
+++ internal/rules/builder.go
@@ -6,9 +6,13 @@ import (
 
 var (
 	Rules = map[string]core.Rule{
-		"COMMENT_ENUM":    &CommentEnum{},
-		"COMMENT_ONEOF":   &CommentOneOf{},
-		"COMMENT_RPC":     &CommentRPC{},
-		"COMMENT_SERVICE": &CommentService{},
+		"COMMENT_ENUM":                &CommentEnum{},
+		"COMMENT_ONEOF":               &CommentOneOf{},
+		"COMMENT_RPC":                 &CommentRPC{},
+		"COMMENT_SERVICE":             &CommentService{},
+		"COMMENT_MESSAGE":             &CommentMessage{},
+		"COMMENT_MESSAGE_FIELD":       &CommentMessageField{},
+		"ENUM_PASCAL_CASE":            &EnumPascalCase{},
+		"ENUM_VALUE_UPPER_SNAKE_CASE": &EnumValueUpperSnakeCase{},
 	}
 )
