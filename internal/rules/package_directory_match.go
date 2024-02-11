bd1c11014cf59e662f5c1e78aa613820cd527742
Edgar Sipki <edo7796@gmail.com>
2024-02-12 00:48:55 +0300

    feat: add PackageDirectoryMatch

--- internal/rules/package_directory_match.go
+++ internal/rules/package_directory_match.go
@@ -1,7 +1,8 @@
 package rules
 
 import (
-	"errors"
+	"path/filepath"
+	"strings"
 
 	"github.com/easyp-tech/easyp/internal/core"
 )
@@ -10,9 +11,25 @@ var _ core.Rule = (*PackageDirectoryMatch)(nil)
 
 // PackageDirectoryMatch is a rule for checking consistency of directory and package names.
 type PackageDirectoryMatch struct {
+	Root string
 }
 
 // Validate implements core.Rule.
-func (d *PackageDirectoryMatch) Validate(info core.ProtoInfo) []error {
-	return []error{errors.New("implements me")}
+func (d *PackageDirectoryMatch) Validate(protoInfo core.ProtoInfo) []error {
+	var res []error
+
+	preparePath := filepath.Dir(strings.TrimPrefix(protoInfo.Path, d.Root))
+	expectedPackage := strings.Replace(preparePath, "/", ".", -1)
+
+	for _, pkgInfo := range protoInfo.Info.ProtoBody.Packages {
+		if pkgInfo.Name != expectedPackage {
+			res = append(res, buildError(pkgInfo.Meta.Pos, protoInfo.Path, core.ErrPackageIsNotMatchedWithPath))
+		}
+	}
+
+	if len(res) == 0 {
+		return nil
+	}
+
+	return res
 }
