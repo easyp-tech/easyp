801e18ea6c7f0c2dcc53241c9d3e5f075c7c9d2a
hound672 <hound672@gmail.com>
2024-02-22 18:42:02 +0300

    base

--- cmd/easyp/main.go
+++ cmd/easyp/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"os"
 
 	"github.com/samber/lo"
@@ -11,7 +12,25 @@ import (
 	"github.com/easyp-tech/easyp/internal/version"
 )
 
+func initLogger() {
+	var levelMapping = map[string]slog.Level{
+		"debug": slog.LevelDebug,
+		"info":  slog.LevelInfo,
+		"warn":  slog.LevelWarn,
+		"error": slog.LevelError,
+	}
+
+	level, ok := levelMapping[os.Getenv("LOG_LEVEL")]
+	if !ok {
+		level = slog.LevelInfo
+	}
+
+	slog.SetLogLoggerLevel(level)
+}
+
 func main() {
+	initLogger()
+
 	app := &cli.App{
 		Name:        "easyp info",
 		HelpName:    "easyp",
