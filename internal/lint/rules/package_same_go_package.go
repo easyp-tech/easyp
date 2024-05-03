d1899543a0816cc78bd5f5dd3e68d0e96f73450c
Edgar Sipki <36490979+ZergsLaw@users.noreply.github.com>
2024-05-03 10:09:00 +0300

    feat: add new linter rules (#23)

---
+++ internal/lint/rules/package_same_go_package.go
@@ -0,0 +1,50 @@
+package rules
+
+import (
+	"github.com/easyp-tech/easyp/internal/lint"
+)
+
+var _ lint.Rule = (*PackageSameGoPackage)(nil)
+
+// PackageSameGoPackage checks that all files with a given package have the same value for the go_package option.
+type PackageSameGoPackage struct {
+	// dir => package
+	cache map[string]string
+}
+
+func (p *PackageSameGoPackage) lazyInit() {
+	if p.cache == nil {
+		p.cache = make(map[string]string)
+	}
+}
+
+// Validate implements lint.Rule.
+func (p *PackageSameGoPackage) Validate(protoInfo lint.ProtoInfo) []error {
+	p.lazyInit()
+
+	var res []error
+
+	if len(protoInfo.Info.ProtoBody.Packages) == 0 {
+		return nil
+	}
+
+	packageName := protoInfo.Info.ProtoBody.Packages[0].Name
+	for _, option := range protoInfo.Info.ProtoBody.Options {
+		if option.OptionName == "go_package" {
+			if p.cache[packageName] == "" {
+				p.cache[packageName] = option.Constant
+				continue
+			}
+
+			if p.cache[packageName] != option.Constant {
+				res = append(res, BuildError(option.Meta.Pos, option.Constant, lint.ErrPackageSameGoPackage))
+			}
+		}
+	}
+
+	if len(res) == 0 {
+		return nil
+	}
+
+	return res
+}
---
+++ internal/lint/rules/package_same_ruby_package_test.go
@@ -0,0 +1,47 @@
+package rules_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/easyp-tech/easyp/internal/lint"
+	"github.com/easyp-tech/easyp/internal/lint/rules"
+)
+
+func TestPackageSameRubyPackage_Validate(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		fileNames []string
+		wantErr   error
+	}{
+		"valid": {
+			fileNames: []string{invalidAuthProto5, invalidAuthProto6},
+			wantErr:   lint.ErrPackageSameRubyPackage,
+		},
+		"invalid": {
+			fileNames: []string{validAuthProto, validAuthProto2},
+			wantErr:   nil,
+		},
+	}
+
+	for name, tc := range tests {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			r, protos := start(t)
+
+			rule := rules.PackageSameRubyPackage{}
+			var got []error
+			for _, fileName := range tc.fileNames {
+				err := rule.Validate(protos[fileName])
+				if len(err) > 0 {
+					got = append(got, err...)
+				}
+			}
+
+			r.ErrorIs(errors.Join(got...), tc.wantErr)
+		})
+	}
+}
