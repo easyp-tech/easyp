e4a5b7041dace35e467631b80c157851cb694457
Vasilii Bliznetsov <hound672@gmail.com>
2024-05-04 13:07:35 +0300

    store

--- internal/api/factories/storage.go
+++ internal/api/factories/storage.go
@@ -1,10 +1,38 @@
 package factories
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/easyp-tech/easyp/internal/mod/adapters/storage"
+)
+
+var (
+	ErrPathNotAbsolute = errors.New("path is not absolute")
 )
 
 func NewStorage() (*storage.Storage, error) {
 	// store := storage.New()
 	return nil, nil
+}
+
+// getEasypPath return path for cache, modules storage
+func getEasypPath() (string, error) {
+	easypPath := os.Getenv(envEasypPath)
+	if easypPath == "" {
+		userHomeDir, err := os.UserHomeDir()
+		if err != nil {
+			return "", fmt.Errorf("os.UserHomeDir: %w", err)
+		}
+		easypPath = filepath.Join(userHomeDir, defaultEasypPath)
+	}
+
+	easypPath, err := filepath.Abs(easypPath)
+	if err != nil {
+		return "", ErrPathNotAbsolute
+	}
+
+	return easypPath, nil
 }
