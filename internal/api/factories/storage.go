247ff8bfe8af3a683483470cf1e4664be4955183
Vasilii Bliznetsov <hound672@gmail.com>
2024-05-04 13:04:54 +0300

    store

---
+++ internal/api/factories/storage.go
@@ -0,0 +1,10 @@
+package factories
+
+import (
+	"github.com/easyp-tech/easyp/internal/mod/adapters/storage"
+)
+
+func NewStorage() (*storage.Storage, error) {
+	// store := storage.New()
+	return nil, nil
+}
