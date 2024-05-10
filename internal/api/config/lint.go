2a68c471c5a6a9b5b585c1c2d081f9e8df51356f
Edgar Sipki <edo7796@gmail.com>
2024-05-10 10:42:40 +0300

    feat: add groups

--- internal/api/config/lint.go
+++ internal/api/config/lint.go
@@ -42,60 +42,92 @@ func (cfg *Config) buildFromUse() ([]lint.Rule, error) {
 	return useRule, nil
 }
 
+const (
+	minGroup      = "MINIMAL"
+	basicGroup    = "BASIC"
+	defaultGroup  = "DEFAULT"
+	commentsGroup = "COMMENTS"
+	unaryRPCGroup = "UNARY_RPC"
+)
+
 func (cfg *Config) unwrapLintGroups() {
 	var res []string
 
 	for _, ruleName := range cfg.Lint.Use {
 		switch ruleName {
-		case "MINIMAL":
-			res = append(res, "DIRECTORY_SAME_PACKAGE")
-			res = append(res, "PACKAGE_DEFINED")
-			res = append(res, "PACKAGE_DIRECTORY_MATCH")
-			res = append(res, "PACKAGE_SAME_DIRECTORY")
-		case "BASIC":
-			res = append(res, "ENUM_FIRST_VALUE_ZERO")
-			res = append(res, "ENUM_NO_ALLOW_ALIAS")
-			res = append(res, "ENUM_PASCAL_CASE")
-			res = append(res, "ENUM_VALUE_UPPER_SNAKE_CASE")
-			res = append(res, "FIELD_LOWER_SNAKE_CASE")
-			res = append(res, "IMPORT_NO_PUBLIC")
-			res = append(res, "IMPORT_NO_WEAK")
-			res = append(res, "IMPORT_USED")
-			res = append(res, "MESSAGE_PASCAL_CASE")
-			res = append(res, "ONEOF_LOWER_SNAKE_CASE")
-			res = append(res, "PACKAGE_LOWER_SNAKE_CASE")
-			res = append(res, "PACKAGE_SAME_CSHARP_NAMESPACE")
-			res = append(res, "PACKAGE_SAME_GO_PACKAGE")
-			res = append(res, "PACKAGE_SAME_JAVA_MULTIPLE_FILES")
-			res = append(res, "PACKAGE_SAME_JAVA_PACKAGE")
-			res = append(res, "PACKAGE_SAME_PHP_NAMESPACE")
-			res = append(res, "PACKAGE_SAME_RUBY_PACKAGE")
-			res = append(res, "PACKAGE_SAME_SWIFT_PREFIX")
-			res = append(res, "RPC_PASCAL_CASE")
-			res = append(res, "SERVICE_PASCAL_CASE")
-
-		case "DEFAULT":
-			res = append(res, "ENUM_VALUE_PREFIX")
-			res = append(res, "ENUM_ZERO_VALUE_SUFFIX")
-			res = append(res, "FILE_LOWER_SNAKE_CASE")
-			res = append(res, "RPC_REQUEST_RESPONSE_UNIQUE")
-			res = append(res, "RPC_REQUEST_STANDARD_NAME")
-			res = append(res, "RPC_RESPONSE_STANDARD_NAME")
-			res = append(res, "PACKAGE_VERSION_SUFFIX")
-			res = append(res, "SERVICE_SUFFIX")
-		case "COMMENTS":
-			res = append(res, "COMMENT_ENUM")
-			res = append(res, "COMMENT_ENUM_VALUE")
-			res = append(res, "COMMENT_FIELD")
-			res = append(res, "COMMENT_MESSAGE")
-			res = append(res, "COMMENT_ONEOF")
-			res = append(res, "COMMENT_RPC")
-			res = append(res, "COMMENT_SERVICE")
-		case "UNARY_RPC":
-			res = append(res, "RPC_NO_CLIENT_STREAMING")
-			res = append(res, "RPC_NO_SERVER_STREAMING")
+		case minGroup:
+			res = cfg.addMinimal(res)
+		case basicGroup:
+			res = cfg.addBasic(res)
+		case defaultGroup:
+			res = cfg.addDefault(res)
+		case commentsGroup:
+			res = cfg.addComments(res)
+		case unaryRPCGroup:
+			res = cfg.addUnary(res)
 		}
 	}
 
 	cfg.Lint.Use = lo.FindUniques(res)
+}
+
+func (cfg *Config) addMinimal(res []string) []string {
+	res = append(res, "DIRECTORY_SAME_PACKAGE")
+	res = append(res, "PACKAGE_DEFINED")
+	res = append(res, "PACKAGE_DIRECTORY_MATCH")
+	res = append(res, "PACKAGE_SAME_DIRECTORY")
+	return res
+}
+
+func (cfg *Config) addBasic(res []string) []string {
+	res = append(res, "ENUM_FIRST_VALUE_ZERO")
+	res = append(res, "ENUM_NO_ALLOW_ALIAS")
+	res = append(res, "ENUM_PASCAL_CASE")
+	res = append(res, "ENUM_VALUE_UPPER_SNAKE_CASE")
+	res = append(res, "FIELD_LOWER_SNAKE_CASE")
+	res = append(res, "IMPORT_NO_PUBLIC")
+	res = append(res, "IMPORT_NO_WEAK")
+	res = append(res, "IMPORT_USED")
+	res = append(res, "MESSAGE_PASCAL_CASE")
+	res = append(res, "ONEOF_LOWER_SNAKE_CASE")
+	res = append(res, "PACKAGE_LOWER_SNAKE_CASE")
+	res = append(res, "PACKAGE_SAME_CSHARP_NAMESPACE")
+	res = append(res, "PACKAGE_SAME_GO_PACKAGE")
+	res = append(res, "PACKAGE_SAME_JAVA_MULTIPLE_FILES")
+	res = append(res, "PACKAGE_SAME_JAVA_PACKAGE")
+	res = append(res, "PACKAGE_SAME_PHP_NAMESPACE")
+	res = append(res, "PACKAGE_SAME_RUBY_PACKAGE")
+	res = append(res, "PACKAGE_SAME_SWIFT_PREFIX")
+	res = append(res, "RPC_PASCAL_CASE")
+	res = append(res, "SERVICE_PASCAL_CASE")
+	return res
+}
+
+func (cfg *Config) addDefault(res []string) []string {
+	res = append(res, "ENUM_VALUE_PREFIX")
+	res = append(res, "ENUM_ZERO_VALUE_SUFFIX")
+	res = append(res, "FILE_LOWER_SNAKE_CASE")
+	res = append(res, "RPC_REQUEST_RESPONSE_UNIQUE")
+	res = append(res, "RPC_REQUEST_STANDARD_NAME")
+	res = append(res, "RPC_RESPONSE_STANDARD_NAME")
+	res = append(res, "PACKAGE_VERSION_SUFFIX")
+	res = append(res, "SERVICE_SUFFIX")
+	return res
+}
+
+func (cfg *Config) addComments(res []string) []string {
+	res = append(res, "COMMENT_ENUM")
+	res = append(res, "COMMENT_ENUM_VALUE")
+	res = append(res, "COMMENT_FIELD")
+	res = append(res, "COMMENT_MESSAGE")
+	res = append(res, "COMMENT_ONEOF")
+	res = append(res, "COMMENT_RPC")
+	res = append(res, "COMMENT_SERVICE")
+	return res
+}
+
+func (cfg *Config) addUnary(res []string) []string {
+	res = append(res, "RPC_NO_CLIENT_STREAMING")
+	res = append(res, "RPC_NO_SERVER_STREAMING")
+	return res
 }
