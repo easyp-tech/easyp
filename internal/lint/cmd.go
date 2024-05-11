e0ad9c95fd09a11cf6e2fbe713850cc6b1e6c0eb
Vasilii Bliznetsov <hound672@gmail.com>
2024-05-11 18:17:46 +0300

    cmd

--- internal/lint/cmd.go
+++ internal/lint/cmd.go
@@ -43,18 +43,15 @@ func (c *Lint) Lint(ctx context.Context, disk fs.FS) error {
 			}
 		}()
 
-		got, err := protoparser.Parse(f)
+		proto, err := readProtoFile(f)
 		if err != nil {
-			return fmt.Errorf("protoparser.Parse: %w", err)
-		}
-
-		proto, err := unordered.InterpretProto(got)
-		if err != nil {
-			return fmt.Errorf("unordered.InterpretProto: %w", err)
+			return fmt.Errorf("readProtoFile: %w", err)
 		}
 
 		// TODO:  read all imported proto files
-		c.readImportedFiles(ctx, disk, proto)
+		if err := c.readFilesFromImport(ctx, disk, proto); err != nil {
+			return fmt.Errorf("readFilesFromImport: %w", err)
+		}
 
 		for i := range c.rules {
 			if ctx.Err() != nil {
@@ -79,32 +76,52 @@ func (c *Lint) Lint(ctx context.Context, disk fs.FS) error {
 	return errors.Join(res...)
 }
 
-// readImportedFiles reads all files that imported from scanning file
-func (c *Lint) readImportedFiles(ctx context.Context, disk fs.FS, scanProto *unordered.Proto) error {
+// readFilesFromImport reads all files that imported from scanning file
+func (c *Lint) readFilesFromImport(ctx context.Context, disk fs.FS, scanProto *unordered.Proto) error {
 	for _, imp := range scanProto.ProtoBody.Imports {
-		importedFile, err := c.readImportedFile(ctx, disk, strings.Trim(imp.Location, "\""))
+		fileFromImport, err := c.readFileFromImport(ctx, disk, strings.Trim(imp.Location, "\""))
 		if err != nil {
-			return fmt.Errorf("readImportedFile: %w", err)
+			return fmt.Errorf("readFileFromImport: %w", err)
 		}
 
-		_ = importedFile
+		_ = fileFromImport
 	}
 
 	return nil
 }
 
-func (c *Lint) readImportedFile(ctx context.Context, disk fs.FS, importName string) (*unordered.Proto, error) {
+func (c *Lint) readFileFromImport(ctx context.Context, disk fs.FS, importName string) (*unordered.Proto, error) {
 	// first try to read it locally
 	f, err := disk.Open(importName)
-	if err != nil {
-		// not locally import
-		modulePath, err := c.moduleReflect.GetModulePath(ctx, importName)
-		_ = modulePath
-		_ = err
+	if err == nil {
+		// locally import
+		defer func() {
+			_ = f.Close()
+		}()
+
+		proto, err := readProtoFile(f)
+		if err != nil {
+			return nil, fmt.Errorf("readProtoFile: %w", err)
+		}
+		return proto, nil
 	}
 
 	_ = f
 	_ = err
 
 	return nil, nil
+}
+
+func readProtoFile(f fs.File) (*unordered.Proto, error) {
+	got, err := protoparser.Parse(f)
+	if err != nil {
+		return nil, fmt.Errorf("protoparser.Parse: %w", err)
+	}
+
+	proto, err := unordered.InterpretProto(got)
+	if err != nil {
+		return nil, fmt.Errorf("unordered.InterpretProto: %w", err)
+	}
+
+	return proto, nil
 }
