5ce240eaaf710657f0cecfac2e801310235665d9
Vasilii Bliznetsov <hound672@gmail.com>
2024-05-11 18:37:47 +0300

    feat: Read files from import

--- internal/lint/lint.go
+++ internal/lint/lint.go
@@ -2,19 +2,27 @@
 package lint
 
 import (
+	"log"
+
 	"github.com/yoheimuta/go-protoparser/v4/interpret/unordered"
+
+	"github.com/easyp-tech/easyp/internal/api/factories"
+	modulereflect "github.com/easyp-tech/easyp/internal/api/shared/module_reflect"
 )
 
 // Lint is the core functionality of easyp lint.
 type Lint struct {
-	rules      []Rule
-	ignoreDirs []string
+	rules         []Rule
+	ignoreDirs    []string
+	deps          []string
+	moduleReflect *modulereflect.ModuleReflect
 }
 
 // ProtoInfo is the information of a proto file.
 type ProtoInfo struct {
-	Path string
-	Info *unordered.Proto
+	Path                 string
+	Info                 *unordered.Proto
+	ProtoFilesFromImport []*unordered.Proto
 }
 
 // Rule is an interface for a rule checking.
@@ -24,9 +32,16 @@ type Rule interface {
 }
 
 // New creates a new Lint.
-func New(rules []Rule, ignoreDirs []string) *Lint {
+func New(rules []Rule, ignoreDirs []string, deps []string) *Lint {
+	moduleReflect, err := factories.NewModuleReflect()
+	if err != nil {
+		log.Fatal(err) // TODO; return error
+	}
+
 	return &Lint{
-		rules:      rules,
-		ignoreDirs: ignoreDirs,
+		rules:         rules,
+		ignoreDirs:    ignoreDirs,
+		deps:          deps,
+		moduleReflect: moduleReflect,
 	}
 }
--- internal/lint/rules/import_used.go
+++ internal/lint/rules/import_used.go
@@ -1,41 +1,75 @@
 package rules
 
 import (
+	"context"
+	"os"
+	"path/filepath"
 	"strings"
 
+	"github.com/yoheimuta/go-protoparser/v4"
+	"github.com/yoheimuta/go-protoparser/v4/interpret/unordered"
 	"github.com/yoheimuta/go-protoparser/v4/parser"
 
+	"github.com/easyp-tech/easyp/internal/api/factories"
+	modulereflect "github.com/easyp-tech/easyp/internal/api/shared/module_reflect"
 	"github.com/easyp-tech/easyp/internal/lint"
 )
 
 var _ lint.Rule = (*ImportUsed)(nil)
 
 // ImportUsed this rule checks that all the imports declared across your Protobuf files are actually used.
-type ImportUsed struct{}
+type ImportUsed struct {
+	deps          []string // FIXME: passed deps is tmp solution!
+	moduleReflect *modulereflect.ModuleReflect
+}
 
 // Validate implements lint.Rule.
-func (i ImportUsed) Validate(protoInfo lint.ProtoInfo) []error {
+func (i *ImportUsed) Validate(protoInfo lint.ProtoInfo) []error {
+	// TODO: tmp solution! only for PoC!
+	var err error
+	i.moduleReflect, err = factories.NewModuleReflect()
+	if err != nil {
+		panic(err)
+	}
+
 	var res []error
 
-	imports := make(map[string]bool)
-	importInfo := make(map[string]*parser.Import)
+	// collects flags if import was used
+	isImportUsed := make(map[string]bool)
+
+	// information about imports (used for linter error message)
+	importsInfo := make(map[string]*parser.Import)
+
+	// imported proto files
+	importsProto := make(map[string]*unordered.Proto)
+
+	// package -> import name
+	pkgProto := make(map[string]string)
+
 	for _, imp := range protoInfo.Info.ProtoBody.Imports {
-		pkgName := i.formatImportName(imp.Location)
-		imports[pkgName] = false
-		importInfo[pkgName] = imp
+		importName := i.formatImportName(imp.Location)
+		isImportUsed[importName] = false
+		importsInfo[importName] = imp
+		importedProto := i.readImportProtoFile(importName)
+		importsProto[importName] = importedProto
+		if len(importedProto.ProtoBody.Packages) == 0 {
+			continue
+		}
+		pkgName := importedProto.ProtoBody.Packages[0].Name
+		pkgProto[pkgName] = importName
 	}
 
 	for _, msg := range protoInfo.Info.ProtoBody.Messages {
 		for _, field := range msg.MessageBody.Fields {
 			key := i.formatField(field.Type)
-			if _, ok := imports[key]; ok {
-				imports[key] = true
+			if _, ok := isImportUsed[key]; ok {
+				isImportUsed[key] = true
 			}
 
 			for i2 := range field.FieldOptions {
 				key = i.formatOption(field.FieldOptions[i2].OptionName)
-				if _, ok := imports[key]; ok {
-					imports[key] = true
+				if _, ok := isImportUsed[key]; ok {
+					isImportUsed[key] = true
 				}
 
 			}
@@ -47,16 +81,16 @@ func (i ImportUsed) Validate(protoInfo lint.ProtoInfo) []error {
 			for _, rpcOption := range rpc.Options {
 				key := i.formatOption(rpcOption.OptionName)
 				key = i.rpcOptionExclusion(key, rpcOption.Constant)
-				if _, ok := imports[key]; ok {
-					imports[key] = true
+				if _, ok := isImportUsed[key]; ok {
+					isImportUsed[key] = true
 				}
 			}
 		}
 	}
 
-	for imp, used := range imports {
+	for imp, used := range isImportUsed {
 		if !used {
-			res = append(res, BuildError(importInfo[imp].Meta.Pos, importInfo[imp].Location, lint.ErrImportIsNotUsed))
+			res = append(res, BuildError(importsInfo[imp].Meta.Pos, importsInfo[imp].Location, lint.ErrImportIsNotUsed))
 		}
 	}
 
@@ -112,4 +146,43 @@ func (i ImportUsed) rpcOptionExclusion(input string, constant string) string {
 	default:
 		return input
 	}
+}
+
+// readImportProtoFile reads imported proto file
+// TODO: need to read from local imports
+func (i ImportUsed) readImportProtoFile(importName string) *unordered.Proto {
+	ctx := context.TODO()
+
+	// try to find actual module
+	for _, dep := range i.deps {
+		modulepath, err := i.moduleReflect.GetModulePath(ctx, dep)
+		if err != nil {
+			panic(err)
+		}
+
+		// if file exists that it's the module that we are looking for
+		fullPath := filepath.Join(modulepath, importName)
+		fp, err := os.Open(fullPath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+			panic(err)
+		}
+
+		got, err := protoparser.Parse(fp)
+		if err != nil {
+			panic(err)
+		}
+		_ = fp.Close()
+
+		proto, err := unordered.InterpretProto(got)
+		if err != nil {
+			panic(err)
+		}
+
+		return proto
+	}
+
+	return nil
 }
