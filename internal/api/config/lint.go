2f9a3086b3f67e30c6236b664115c414bd3da1f3
Edgar Sipki <edo7796@gmail.com>
2024-06-12 00:55:41 +0300

    feat[66]: add AllowCommentIgnores

--- internal/api/config/lint.go
+++ internal/api/config/lint.go
@@ -11,11 +11,12 @@ import (
 
 // LintConfig contains linter configuration.
 type LintConfig struct {
-	Use                       []string `json:"use" yaml:"use" env:"USE"`                                                                       // Use rules for linter.
-	EnumZeroValueSuffixPrefix string   `json:"enumZeroValueSuffixPrefix" yaml:"enumZeroValueSuffixPrefix" env:"ENUM_ZERO_VALUE_SUFFIX_PREFIX"` // Enum zero value suffix prefix.
-	ServiceSuffixSuffix       string   `json:"serviceSuffixSuffix" yaml:"serviceSuffixSuffix" env:"SERVICE_SUFFIX_SUFFIX"`                     // Service suffix suffix.
-	Ignore                    []string `json:"ignore" yaml:"ignore" env:"IGNORE"`                                                              // Ignore dirs with proto file.
-	Except                    []string `json:"except" yaml:"except" env:"EXCEPT"`                                                              // Except linter rules.
+	Use                 []string `json:"use" yaml:"use" env:"USE"`                                                          // Use rules for linter.
+	EnumZeroValueSuffix string   `json:"enum_zero_value_suffix" yaml:"enum_zero_value_suffix" env:"ENUM_ZERO_VALUE_SUFFIX"` // Enum zero value suffix prefix.
+	ServiceSuffix       string   `json:"service_suffix" yaml:"service_suffix" env:"SERVICE_SUFFIX"`                         // Service suffix suffix.
+	Ignore              []string `json:"ignore" yaml:"ignore" env:"IGNORE"`                                                 // Ignore dirs with proto file.
+	Except              []string `json:"except" yaml:"except" env:"EXCEPT"`                                                 // Except linter rules.
+	AllowCommentIgnores bool     `json:"allow_comment_ignores" yaml:"allow_comment_ignores" env:"ALLOW_COMMENT_IGNORES"`    // Allow comment ignore.
 }
 
 func (cfg *Config) BuildLinterRules() ([]lint.Rule, error) {
