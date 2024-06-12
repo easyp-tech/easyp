bba08d826f2a9a9864340406f454a397afe32735
hound672 <hound672@gmail.com>
2024-06-12 22:54:04 +0300

    Add

--- internal/lint/rules/check_lint_ignore.go
+++ internal/lint/rules/check_lint_ignore.go
@@ -1,10 +1,28 @@
 package rules
 
 import (
+	"strings"
+
 	"github.com/yoheimuta/go-protoparser/v4/parser"
+)
+
+const (
+	bufLintIgnorePrefix = "buf:lint:ignore "
 )
 
 // CheckIsIgnored check if passed ruleName has to be ignored due to ignore command in comments
 func CheckIsIgnored(comments []parser.Comment, ruleName string) bool {
+	if len(comments) == 0 {
+		return false
+	}
+
+	s := bufLintIgnorePrefix + ruleName
+
+	for _, comment := range comments {
+		if strings.Contains(comment.Raw, s) {
+			return true
+		}
+	}
+
 	return false
 }
