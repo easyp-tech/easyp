eb2ebbfce3724b1d8625855b13d34c798e135225
Edgar Sipki <edo7796@gmail.com>
2024-07-28 15:51:40 +0300

    bug: fix wlk type and add fix for rpc-pascal-case checker

--- internal/lint/rules/rpc_pascal_case.go
+++ internal/lint/rules/rpc_pascal_case.go
@@ -1,6 +1,7 @@
 package rules
 
 import (
+	"fmt"
 	"regexp"
 
 	"github.com/easyp-tech/easyp/internal/lint"
@@ -19,10 +20,11 @@ func (c *RPCPascalCase) Message() string {
 // Validate implements lint.Rule.
 func (c *RPCPascalCase) Validate(protoInfo lint.ProtoInfo) ([]lint.Issue, error) {
 	var res []lint.Issue
-	pascalCase := regexp.MustCompile("^[A-Z][a-z]+([A-Z][a-z]+)*$")
+	pascalCase := regexp.MustCompile("^[A-Z][a-zA-Z0-9]*$")
 	for _, service := range protoInfo.Info.ProtoBody.Services {
 		for _, rpc := range service.ServiceBody.RPCs {
 			if !pascalCase.MatchString(rpc.RPCName) {
+				fmt.Println("RPC name: ", rpc.RPCName)
 				res = lint.AppendIssue(res, c, rpc.Meta.Pos, rpc.RPCName, rpc.Comments)
 			}
 		}
