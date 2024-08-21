bc4b53508f1d62216228a80873ad06077ced444c
hound672 <hound672@gmail.com>
2024-08-21 13:05:00 +0300

    Add dependencies slice

--- internal/mod/models/module_config.go
+++ internal/mod/models/module_config.go
@@ -4,4 +4,8 @@ package models
 type ModuleConfig struct {
 	// Directories contains dirs with proto files (buf.work.yaml)
 	Directories []string
+
+	// Dependencies contains list of required dependencies in repository
+	// it could be from easyp.yaml or from buf
+	Dependencies []Module
 }
