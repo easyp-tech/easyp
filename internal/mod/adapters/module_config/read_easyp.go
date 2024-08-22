71f1b18a27a0789f2f49aeddd4be0c1e4a2943ad
Vasilii Bliznetsov <hound672@gmail.com>
2024-08-22 19:04:53 +0300

    Add base

---
+++ internal/mod/adapters/module_config/read_easyp.go
@@ -0,0 +1,13 @@
+package moduleconfig
+
+import (
+	"context"
+
+	"github.com/easyp-tech/easyp/internal/mod/adapters/repository"
+	"github.com/easyp-tech/easyp/internal/mod/models"
+)
+
+// readEasyp read easyp's config from repository
+func readEasyp(ctx context.Context, repo repository.Repo, revision models.Revision) {
+
+}
