2bfe020a4a7a268aa442f08ed42c79098f9da977
Vasilii Bliznetsov <VBliznetsov@alfabank.ru>
2024-12-19 14:42:20 +0300

    Remove not used func

--- internal/mod/download_test.go
+++ internal/mod/download_test.go
@@ -12,39 +12,3 @@ func getFakeModule() models.Module {
 
 	return module
 }
-
-func (s *modSuite) Test_getVersionToInstall_NoInLockFile() {
-	module := getFakeModule()
-
-	s.lockFile.EXPECT().Read(module.Name).Return(models.LockFileInfo{}, models.ErrModuleNotFoundInLockFile)
-
-	res, err := s.mod.getVersionToDownload(module)
-
-	s.NoError(err)
-	s.Equal(module.Version, res)
-}
-
-func (s *modSuite) Test_getVersionToInstall_InLockFile() {
-	module := getFakeModule()
-	lockFileInfo := models.LockFileInfo{}
-	_ = gofakeit.Struct(&lockFileInfo)
-
-	s.lockFile.EXPECT().Read(module.Name).Return(lockFileInfo, nil)
-
-	res, err := s.mod.getVersionToDownload(module)
-
-	s.NoError(err)
-	s.Equal(models.RequestedVersion(lockFileInfo.Version), res)
-}
-
-func (s *modSuite) Test_getVersionToInstall_ErrorReadLockFile() {
-	module := getFakeModule()
-	rErr := gofakeit.Error()
-
-	s.lockFile.EXPECT().Read(module.Name).Return(models.LockFileInfo{}, rErr)
-
-	res, err := s.mod.getVersionToDownload(module)
-
-	s.Empty(res)
-	s.ErrorIs(err, rErr)
-}
