a29ae12c0788f1e9ffd5d0a19970ae7d1b68f5e0
Vasilii Bliznetsov <VBliznetsov@alfabank.ru>
2024-12-24 15:23:23 +0300

    Add command

--- internal/api/mod.go
+++ internal/api/mod.go
@@ -40,6 +40,13 @@ func (m Mod) Command() *cli.Command {
 		Description: "update modules version using version from config",
 		Action:      m.Update,
 	}
+	vendorCmd := &cli.Command{
+		Name:        "vendor",
+		Usage:       "copy proto files from deps to vendor dir",
+		UsageText:   "copy proto files from deps to vendor dir",
+		Description: "copy proto files from deps to vendor dir",
+		Action:      m.Vendor,
+	}
 
 	return &cli.Command{
 		Name:                   "mod",
@@ -54,7 +61,7 @@ func (m Mod) Command() *cli.Command {
 		After:                  nil,
 		Action:                 nil,
 		OnUsageError:           nil,
-		Subcommands:            []*cli.Command{downloadCmd, updateCmd},
+		Subcommands:            []*cli.Command{downloadCmd, updateCmd, vendorCmd},
 		Flags:                  []cli.Flag{},
 		SkipFlagParsing:        false,
 		HideHelp:               false,
@@ -185,6 +192,70 @@ func (m Mod) Update(ctx *cli.Context) error {
 	)
 
 	if err := app.Update(ctx.Context, cfg.Deps); err != nil {
+		if errors.Is(err, models.ErrVersionNotFound) {
+			os.Exit(1)
+		}
+
+		return fmt.Errorf("cmd.Download: %w", err)
+	}
+	return nil
+}
+
+func (m Mod) Vendor(ctx *cli.Context) error {
+	cfg, err := config.ReadConfig(ctx)
+	if err != nil {
+		return fmt.Errorf("config.ReadConfig: %w", err)
+	}
+	core.SetAllowCommentIgnores(cfg.Lint.AllowCommentIgnores)
+
+	lintRules, err := cfg.BuildLinterRules()
+	if err != nil {
+		return fmt.Errorf("cfg.BuildLinterRules: %w", err)
+	}
+
+	moduleReflect, err := factories.NewModuleReflect()
+	if err != nil {
+		return fmt.Errorf("factories.NewModuleReflect: %w", err)
+	}
+
+	lockFile := lockfile.New()
+	easypPath, err := getEasypPath()
+	if err != nil {
+		return fmt.Errorf("getEasypPath: %w", err)
+	}
+
+	store := storage.New(easypPath, lockFile)
+
+	moduleCfg := moduleconfig.New()
+
+	app := core.New(
+		lintRules,
+		cfg.Lint.Ignore,
+		cfg.Deps,
+		moduleReflect,
+		cfg.Lint.IgnoreOnly,
+		slog.Default(), // TODO: remove global state
+		lo.Map(cfg.Generate.Plugins, func(p config.Plugin, _ int) core.Plugin {
+			return core.Plugin{
+				Name:    p.Name,
+				Out:     p.Out,
+				Options: p.Opts,
+			}
+		}),
+		core.Inputs{
+			Dirs: lo.Filter(lo.Map(cfg.Generate.Inputs, func(i config.Input, _ int) string {
+				return i.Directory
+			}), func(s string, _ int) bool {
+				return s != ""
+			}),
+		},
+		console.New(),
+		store,
+		moduleCfg,
+		lockFile,
+	)
+
+	if err := app.Vendor(ctx.Context); err != nil {
 		if errors.Is(err, models.ErrVersionNotFound) {
 			os.Exit(1)
 		}
