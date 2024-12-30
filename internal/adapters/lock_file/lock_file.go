974e03eaaf6a72dd432a709c83f73cace49bee86
Vasilii Bliznetsov <hound672@gmail.com>
2024-12-30 13:21:46 +0300

    chore: Use DirWalker for lock faile

--- internal/adapters/lock_file/lock_file.go
+++ internal/adapters/lock_file/lock_file.go
@@ -2,8 +2,6 @@ package lockfile
 
 import (
 	"bufio"
-	"log"
-	"os"
 	"strings"
 
 	"github.com/easyp-tech/easyp/internal/core"
@@ -21,39 +19,33 @@ type fileInfo struct {
 
 type LockFile struct {
 	dirWalker core.DirWalker
-
-	fp    *os.File
-	cache map[string]fileInfo
+	cache     map[string]fileInfo
 }
 
 func New(dirWalker core.DirWalker) *LockFile {
-	fp, err := os.OpenFile(lockFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, lockFilePerms)
-	if err != nil {
-		// TODO: return err?
-		log.Fatal(err)
-	}
-
 	cache := make(map[string]fileInfo)
 
-	fscanner := bufio.NewScanner(fp)
-	for fscanner.Scan() {
-		parts := strings.Fields(fscanner.Text())
-		if len(parts) != 3 {
-			continue
-		}
+	fp, err := dirWalker.Open(lockFileName)
+	if err == nil {
+		fscanner := bufio.NewScanner(fp)
 
-		fileInfo := fileInfo{
-			version: parts[1],
-			hash:    parts[2],
+		for fscanner.Scan() {
+			parts := strings.Fields(fscanner.Text())
+			if len(parts) != 3 {
+				continue
+			}
+
+			fileInfo := fileInfo{
+				version: parts[1],
+				hash:    parts[2],
+			}
+			cache[parts[0]] = fileInfo
 		}
-		cache[parts[0]] = fileInfo
 	}
 
 	lockFile := &LockFile{
 		dirWalker: dirWalker,
-
-		fp:    fp,
-		cache: cache,
+		cache:     cache,
 	}
 	return lockFile
 }
