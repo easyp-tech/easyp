40c77ddc30923da9b60c41cc667c39c8563aeda9
Edgar Sipki <edo7796@gmail.com>
2025-11-23 18:20:58 +0300

    docs: fix

--- docs/dist/assets/stateDiagram-v2-4FDKWEC3-B6U4gryh.js
+++
@@ -1,1 +0,0 @@
-import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-D7dEsI1L.js";import{_ as i}from"./index-BL64cVGB.js";import"./chunk-55IACEB6-lK9JNFG_.js";import"./chunk-QN33PNHL-CsxJ0oCs.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
