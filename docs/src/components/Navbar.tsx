0007c6f41e7520539198308f294ce9bb01b7b6e5
Edgar Sipki <edo7796@gmail.com>
2025-12-01 00:25:30 +0300

    happy new year: add holida spirit :)

--- docs/src/components/Navbar.tsx
+++ docs/src/components/Navbar.tsx
@@ -33,6 +33,7 @@ export default function Navbar() {
                                 <span className="font-bold text-primary text-sm">EP</span>
                             </div>
                             <span className="font-semibold text-lg tracking-tight text-white">EasyP</span>
+                            <span className="text-green-400 text-sm">🎄</span>
                         </div>
 
                         <a
