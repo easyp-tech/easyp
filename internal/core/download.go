858392fa1686d442265870e5c80db3901a4c35d1
Vasilii Bliznetsov <hound672@gmail.com>
2025-12-16 14:41:31 +0300

    base for download

--- internal/core/download.go
+++ internal/core/download.go
@@ -13,6 +13,39 @@ import (
 // dependencies slice of strings format: origin@version: github.com/company/repository@v1.2.3
 // if version is absent use the latest commit
 func (c *Core) Download(ctx context.Context, dependencies []string) error {
+	c.logger.DebugContext(ctx, "start download")
+
+	// install from lock file at first
+	for lockFileInfo := range c.lockFile.DepsIter() {
+		module := models.NewModuleFromLockFileInfo(lockFileInfo)
+
+		c.logger.DebugContext(
+			ctx, "start download module from lockfile", "name", module.Name, "version", module.Version,
+		)
+	}
+
+	// install from remote generator sections
+	for _, dependency := range dependencies {
+		module := models.NewModule(dependency)
+
+		_, err := c.lockFile.Read(module.Name)
+		if err == nil {
+			c.logger.DebugContext(
+				ctx, "already is in lock file", "name", module.Name, "version", module.Version,
+			)
+			continue
+		}
+		if !errors.Is(err, models.ErrModuleNotFoundInLockFile) {
+			return fmt.Errorf("c.lockFile.Read: %w", err)
+		}
+
+		c.logger.DebugContext(
+			ctx, "start download module from deps", "name", module.Name, "version", module.Version,
+		)
+	}
+
+	return nil
+
 	if c.lockFile.IsEmpty() {
 		// if lock file is empty or doesn't exist install versions
 		// from easyp.yaml config and create lock file
